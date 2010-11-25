d90f2a2f2500e4a293a84a84f6291e65ee65b999
Michael Contento <michaelcontento@gmail.com>
2010-11-25 12:52:08 -0800

    unneeded imports removed

--- example/hello_world.py
+++ example/hello_world.py
@@ -3,7 +3,6 @@ import os
 sys.path.insert(0, os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]), os.pardir, os.pardir)))
 
 from traffic_light import server
-from traffic_light.job import CIJoe, Clear
-from traffic_light.event import Text, NextUpdate
+from traffic_light.event import Text
 
 server.run(('', 8804), [Text('Hallo Welt')])  
