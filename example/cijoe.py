fd8c677f0f9a3e68d5ad7c2d36a1937756205498
Michael Contento <michaelcontento@gmail.com>
2010-11-25 16:48:04 +0100

    example.py splitted into 2 files in example/

---
+++ example/cijoe.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]), os.pardir, os.pardir)))
+
+from traffic_light import server
+from traffic_light.job import CIJoe, Clear
+from traffic_light.event import Text, NextUpdate
+
+server.run(('', 8804), [CIJoe('localhost', 4567, 'CIJoe on localhost')])
